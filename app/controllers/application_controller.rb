a75e661f37dd0bca677626d8f0e4a5da80d8158b
Prashanth <a.prashanth18@gmail.com>
2020-12-20 22:37:52 +0530

    allow api setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   def fallback_index_html
     render :file => 'public/index.html'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :lists
     end
   end
-  root to: 'application#fallback_index_html'
+
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
